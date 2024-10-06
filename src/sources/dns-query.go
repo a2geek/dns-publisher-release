3edcd4013eb60073898e2c17c43d7ef5584c35e9
Rob Greene <robgreene@gmail.com>
2024-10-06 11:08:36 -0500

    Ensuring the DNS queries go against BOSH DNS on 169.254.0.2.

--- src/sources/dns-query.go
+++ src/sources/dns-query.go
@@ -1,20 +1,31 @@
 package sources
 
 import (
+	"context"
 	"net"
 	"time"
 )
 
 func NewDnsQuerySource() (Source, error) {
-	return &dnsQuerySource{}, nil
+	dialFunc := func(c context.Context, network string, _ string) (net.Conn, error) {
+		d := net.Dialer{}
+		return d.DialContext(c, network, "169.254.0.2:53")
+	}
+	return &dnsQuerySource{
+		resolver: net.Resolver{
+			PreferGo: true,
+			Dial:     dialFunc,
+		},
+	}, nil
 }
 
 type dnsQuerySource struct {
+	resolver net.Resolver
 }
 type dnsTick struct {
 	data time.Time
 }
 
 func (s *dnsQuerySource) Lookup(query string) ([]net.IP, error) {
-	return net.LookupIP(query)
+	return s.resolver.LookupIP(context.Background(), "ip4", query)
 }
