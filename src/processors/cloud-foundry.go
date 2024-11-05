ea5700cb066096b07c7f0e34c021d027f6989a9a
Rob Greene <robgreene@gmail.com>
2024-11-05 10:49:44 -0600

    Restructured event processing to a function that can exit if errors start occurring. #1

--- src/processors/cloud-foundry.go
+++ src/processors/cloud-foundry.go
@@ -22,6 +22,7 @@ type cloudFoundryProcessor struct {
 	regexps   []*regexp.Regexp
 	publisher publishers.AliasPublisher
 	logger    boshlog.Logger
+	priorRun  time.Time
 }
 
 const (
@@ -36,107 +37,112 @@ func (p *cloudFoundryProcessor) Run() {
 		os.Exit(1)
 	}
 
-	var priorRun time.Time
 	for range events {
-		p.logger.Debug("cloud-foundry", "checking at %s", time.Now())
-		opts := client.NewAuditEventListOptions()
-		if !priorRun.IsZero() {
-			opts.ListOptions.CreateAts = client.TimestampFilter{
-				Timestamp: []time.Time{priorRun},
-				Operator:  client.FilterModifierGreaterThan,
+		p.checkEvents()
+	}
+}
+
+func (p *cloudFoundryProcessor) checkEvents() {
+	p.logger.Debug("cloud-foundry", "checking at %s", time.Now())
+	opts := client.NewAuditEventListOptions()
+	if !p.priorRun.IsZero() {
+		opts.ListOptions.CreateAts = client.TimestampFilter{
+			Timestamp: []time.Time{p.priorRun},
+			Operator:  client.FilterModifierGreaterThan,
+		}
+	}
+	opts.Types = client.Filter{
+		Values: []string{ROUTE_CREATE, ROUTE_DELETE},
+	}
+
+	p.priorRun = time.Now()
+	urls := map[string]string{}    // GUID:URL
+	actions := map[string]string{} // URL:action
+
+	for {
+		events, pager, err := p.cf.AuditEvents.List(context.Background(), opts)
+		if err != nil {
+			p.logger.Error("cloud-foundry", "error retrieving audit events: %s", err.Error())
+			return
+		}
+
+		for _, event := range events {
+			guid := event.Target.GUID
+
+			url, ok := urls[guid]
+			if !ok {
+				route, err := p.cf.Routes.Get(context.Background(), guid)
+				if err != nil {
+					if resource.IsResourceNotFoundError(err) {
+						url = ""
+						urls[guid] = url
+						p.logger.Warn("cloud-foundry", "route not found: %v", err)
+						continue
+					}
+					p.logger.Error("cloud-foundry", "error retrieving route: %v", err)
+					return
+				}
+				p.logger.Info("cloud-foundry", "route is '%s' for GUID '%s'", route.URL, guid)
+				url = route.URL
+				urls[guid] = url
 			}
-		}
-		opts.Types = client.Filter{
-			Values: []string{ROUTE_CREATE, ROUTE_DELETE},
-		}
 
-		priorRun = time.Now()
-		urls := map[string]string{}    // GUID:URL
-		actions := map[string]string{} // URL:action
-
-		for {
-			events, pager, err := p.cf.AuditEvents.List(context.Background(), opts)
-			if err != nil {
-				p.logger.Error("cloud-foundry", "error retrieving audit events: %s", err.Error())
+			if url == "" {
+				p.logger.Debug("cloud-foundry", "route is undefined for guid %s", guid)
 				continue
 			}
 
-			for _, event := range events {
-				guid := event.Target.GUID
-
-				url, ok := urls[guid]
-				if !ok {
-					route, err := p.cf.Routes.Get(context.Background(), guid)
-					if err != nil {
-						if resource.IsResourceNotFoundError(err) {
-							url = ""
-							urls[guid] = url
-							p.logger.Warn("cloud-foundry", "route not found: %v", err)
-							continue
-						}
-						p.logger.Error("cloud-foundry", "error retrieving route: %v", err)
-						break
-					}
-					p.logger.Info("cloud-foundry", "route is '%s' for GUID '%s'", route.URL, guid)
-					url = route.URL
-					urls[guid] = url
-				}
-
-				if url == "" {
-					p.logger.Debug("cloud-foundry", "route is undefined for guid %s", guid)
-					continue
-				}
-
-				match := false
-				for _, re := range p.regexps {
-					if re.Match([]byte(url)) {
-						match = true
-						p.logger.Debug("cloud-foundry", "route match: '%s' with '%s'", url, re.String())
-					}
-				}
-				if !match {
-					p.logger.Debug("cloud-foundry", "DID NOT MATCH: '%s'", url)
-					continue
-				}
-
-				actions[url] = event.Type
-			}
-
-			if !pager.HasNextPage() {
-				break
-			}
-			pager.NextPage(opts)
-		}
-
-		current, err := p.publisher.Current()
-		if err != nil {
-			p.logger.Error("cloud-foundry", "cannot retrieve current state: %v", err)
-			continue
-		}
-
-		slices.Sort(current)
-		changed := false
-		for url, action := range actions {
-			i := sort.SearchStrings(current, url)
-			exists := i < len(current) && current[i] == url
-			p.logger.Debug("cloud-foundry", "found '%s' in slice '%s': %t (at %d)", url, current, exists, i)
-			switch action {
-			case ROUTE_CREATE:
-				if !exists {
-					p.logger.Info("cloud-foundry", "making alias for '%s' to '%s'", url, p.alias)
-					p.publisher.Add(url, p.alias)
-					changed = true
-				}
-			case ROUTE_DELETE:
-				if exists {
-					p.logger.Info("cloud-foundry", "removing alias for '%s'", url)
-					p.publisher.Delete(url)
-					changed = true
+			match := false
+			for _, re := range p.regexps {
+				if re.Match([]byte(url)) {
+					match = true
+					p.logger.Debug("cloud-foundry", "route match: '%s' with '%s'", url, re.String())
 				}
 			}
+			if !match {
+				p.logger.Debug("cloud-foundry", "DID NOT MATCH: '%s'", url)
+				continue
+			}
+
+			actions[url] = event.Type
 		}
-		if changed {
-			p.publisher.Commit()
+
+		if !pager.HasNextPage() {
+			break
 		}
+		pager.NextPage(opts)
+	}
+
+	current, err := p.publisher.Current()
+	if err != nil {
+		p.logger.Error("cloud-foundry", "cannot retrieve current state: %v", err)
+		return
+	}
+	p.logger.Debug("cloud-foundry", "current alias listing: %v", current)
+	p.logger.Debug("cloud-foundry", "actions captures: %v", actions)
+
+	slices.Sort(current)
+	changed := false
+	for url, action := range actions {
+		i := sort.SearchStrings(current, url)
+		exists := i < len(current) && current[i] == url
+		p.logger.Debug("cloud-foundry", "found '%s' in slice '%s': %t (at %d)", url, current, exists, i)
+		switch action {
+		case ROUTE_CREATE:
+			if !exists {
+				p.logger.Info("cloud-foundry", "making alias for '%s' to '%s'", url, p.alias)
+				p.publisher.Add(url, p.alias)
+				changed = true
+			}
+		case ROUTE_DELETE:
+			if exists {
+				p.logger.Info("cloud-foundry", "removing alias for '%s'", url)
+				p.publisher.Delete(url)
+				changed = true
+			}
+		}
+	}
+	if changed {
+		p.publisher.Commit()
 	}
 }
